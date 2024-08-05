234c51144068d0169a4a6b03841c142bc58c5711
salammuloh <125520973+salammuloh@users.noreply.github.com>
2024-08-05 14:23:15 +0700

    git merge ilham

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e9ec31691d85fe7b0c05c9329194fdb4e53cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1831ec57d450e6a999b923aa97ea6355e7cbb673")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
