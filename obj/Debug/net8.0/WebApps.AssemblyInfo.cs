a1d5547a0ec232248852ccf32e54fbc738b74131
chesukasuka <54785405+chesukasuka@users.noreply.github.com>
2024-08-14 20:55:25 +0700

    udpate

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd064616c11ca91df24f255a3a33805721bcac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e1b2c45ef6c6370747285c9a488e9e6cc253689")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
