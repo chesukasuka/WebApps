a2787c5cc4ee695aecba94953b4b038708d14868
IchwanLatif17 <haurazeynaaskadina@gmail.com>
2024-09-24 00:58:08 +0700

    done responsive benchmarking

--- wwwroot/js/controllers/service.js
+++ wwwroot/js/controllers/service.js
@@ -196,17 +196,15 @@ function loadCustomers() {
             testedparty.value = (penjualan.value - hargapokokpenjualan.value - bebanoperasional.value) / penjualan.value;
             testedparty.value = Math.round(testedparty.value * 100) / 100
         }
+        document.getElementById("jeniskegiatanusaharesult").innerHTML = jenis.value;
+        document.getElementById("klasifikasiusaharesult").innerHTML = klasifikasi.value;
+        document.getElementById("ratioresult").innerHTML = rasio.value;
+        document.getElementById("testedpartyresult").innerHTML = testedparty.value + ' %';
+        document.getElementById("tahunpajakresult").innerHTML = tahunpajak;
     }
     else {
         toastObj.show();
     }
-
-    //console.log("test");
-    //document.getElementById("jeniskegiatanusaharesult").innerHTML = jenis.value;
-    //document.getElementById("klasifikasiusaharesult").innerHTML = klasifikasi.value;
-    //document.getElementById("ratioresult").innerHTML = rasio.value;
-    //document.getElementById("testedpartyresult").innerHTML = testedparty.value + ' %';
-    //document.getElementById("tahunpajakresult").innerHTML = tahunpajak;
 
 
 };
