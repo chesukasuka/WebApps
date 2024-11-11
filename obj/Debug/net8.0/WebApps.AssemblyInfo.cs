e23602bb21fcff67c3a56caae249435265409a82
chesukasuka <chesu.ngeped@gmail.com>
2024-11-11 21:03:03 +0700

    update posisi

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c3ca9fc8bd10f02cf374468006b8e43d86fe79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a881150cc7321ddfa56da73cb20a80f28e5c125")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
