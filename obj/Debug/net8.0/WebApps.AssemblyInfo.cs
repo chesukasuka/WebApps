cbe407e2af5dc55cb38fc627a0c18c31edf8e39a
chesukasuka <chesu.ngeped@gmail.com>
2024-11-21 20:33:29 +0700

    a

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67710ed03fb2fc89afb9a7fbff89befeb7175d07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4cb58182fdeb4d91bc522b929bef133a79a8f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
