c41822a56ebb5d345f7594c8135caed3773ea56d
chesukasuka <chesu.ngeped@gmail.com>
2025-05-22 16:36:24 +0700

    modified:   obj/Debug/net8.0/WebApps.AssemblyInfo.cs modified:   obj/Debug/net8.0/WebApps.AssemblyInfoInputs.cache modified:   VIEWS/SERVICE/Benchmarking.cshtml modified:   VIEWS/SHARED/_Layout.cshtml

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b195d1719697362a38e31f30bd3cbc6098969f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4366ad170322f47bcd56e45b27919fd61b09964d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
