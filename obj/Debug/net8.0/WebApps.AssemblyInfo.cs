95549d995d76286bc293402de0cdfcbd90fdf33f
Rameyza Alya Mukhbita <Sanreyza@gmail.com>
2025-09-01 13:32:02 +0700

    modified:   PROPERTIES/launchSettings.json 	modified:   VIEWS/SHARED/_Layout.cshtml 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfo.cs 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfoInputs.cache 	modified:   obj/Debug/net8.0/WebApps.GeneratedMSBuildEditorConfig.editorconfig 	modified:   obj/Debug/net8.0/WebApps.RazorAssemblyInfo.cs 	modified:   obj/Debug/net8.0/WebApps.assets.cache 	modified:   obj/Debug/net8.0/WebApps.csproj.CoreCompileInputs.cache 	modified:   obj/Debug/net8.0/WebApps.csproj.FileListAbsolute.txt 	modified:   obj/WebApps.csproj.nuget.dgspec.json 	modified:   obj/WebApps.csproj.nuget.g.props 	modified:   obj/project.assets.json 	modified:   obj/project.nuget.cache

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade9e8bd0a6e6744b833a91341da84ff821c0b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a0f184f309576c35a566fe1a97c849b82ead38")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
