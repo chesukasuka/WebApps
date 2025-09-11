47bb3188090d25ff41c6d901fbc40341043d4c95
Rameyza Alya Mukhbita <Sanreyza@gmail.com>
2025-09-11 15:15:18 +0700

    modified:   VIEWS/SHARED/_Layout.cshtml 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfo.cs 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfoInputs.cache

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62dbef3661fbe200f0b7175eb95090190627a21c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3997f04592f5aa69401b1c19461fdd85b5ff58cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
