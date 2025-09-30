8765cf424cb43faee589b5dd3d32c389b43d74d5
Rameyza Alya Mukhbita <Sanreyza@gmail.com>
2025-09-30 16:14:05 +0700

    modified:   VIEWS/SERVICE/Benchmarking.cshtml 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfo.cs 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfoInputs.cache

--- obj/Debug/net8.0/WebApps.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab873a13fe984d5351f75c7ce597937f639a03f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc1acb66ebee452f6878c5b30cc5d893fd3d777")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
