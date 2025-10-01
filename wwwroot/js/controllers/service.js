8f414336bfe225ed3cd79c282bf7788e34b5c066
chesukasuka <chesu.ngeped@gmail.com>
2025-10-01 11:27:42 +0700

    modified:   VIEWS/SERVICE/Benchmarking.cshtml 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfo.cs 	modified:   obj/Debug/net8.0/WebApps.AssemblyInfoInputs.cache 	modified:   obj/Debug/net8.0/WebApps.GeneratedMSBuildEditorConfig.editorconfig 	modified:   obj/Debug/net8.0/WebApps.assets.cache 	modified:   obj/Debug/net8.0/WebApps.csproj.CoreCompileInputs.cache 	modified:   obj/Debug/net8.0/WebApps.csproj.FileListAbsolute.txt 	modified:   obj/WebApps.csproj.nuget.dgspec.json 	modified:   obj/WebApps.csproj.nuget.g.props 	modified:   obj/project.assets.json 	modified:   obj/project.nuget.cache 	modified:   wwwroot/js/controllers/service.js
    Changes not staged for commit:
    	modified:   VIEWS/SERVICE/Benchmarking.cshtml

--- wwwroot/js/controllers/service.js
+++ wwwroot/js/controllers/service.js
@@ -528,265 +528,265 @@ function loadCustomers() {
   }
 }
 
-function loadCustomers() {
-  btoastbefore = true;
-  const elements = document.querySelectorAll(".to-hide"); // Only target elements with the 'to-hide' class
-  elements.forEach((element) => {
-    element.classList.add("hidden");
-  });
+// function loadCustomers() {
+//   btoastbefore = true;
+//   const elements = document.querySelectorAll(".to-hide"); // Only target elements with the 'to-hide' class
+//   elements.forEach((element) => {
+//     element.classList.add("hidden");
+//   });
 
-  const elements2 = document.querySelectorAll(".to-show"); // Only target elements with the 'to-hide' class
-  elements2.forEach((elements2) => {
-    elements2.classList.remove("hidden");
-  });
+//   const elements2 = document.querySelectorAll(".to-show"); // Only target elements with the 'to-hide' class
+//   elements2.forEach((elements2) => {
+//     elements2.classList.remove("hidden");
+//   });
 
-  //var grid = document.getElementById("Grid").ej2_instances[0];
-  var rasio = document.getElementById("ratio").ej2_instances[0];
-  var klasifikasi =
-    document.getElementById("klasifikasiusaha").ej2_instances[0];
-  var subklasifikasi = document.getElementById("subklasifikasiusaha")
-    .ej2_instances[0];
-  var jenis = document.getElementById("jeniskegiatanusaha").ej2_instances[0];
-  // var tahunslider = document.getElementById("tahun").ej2_instances[0];
-  var tahunpajak = document.getElementById("tahunpajak").ej2_instances[0].value;
-  var tahun = document.getElementById("tahun").ej2_instances[0].value;
+//   //var grid = document.getElementById("Grid").ej2_instances[0];
+//   var rasio = document.getElementById("ratio").ej2_instances[0];
+//   var klasifikasi =
+//     document.getElementById("klasifikasiusaha").ej2_instances[0];
+//   var subklasifikasi = document.getElementById("subklasifikasiusaha")
+//     .ej2_instances[0];
+//   var jenis = document.getElementById("jeniskegiatanusaha").ej2_instances[0];
+//   // var tahunslider = document.getElementById("tahun").ej2_instances[0];
+//   var tahunpajak = document.getElementById("tahunpajak").ej2_instances[0].value;
+//   var tahun = document.getElementById("tahun").ej2_instances[0].value;
 
-  var tahun1 = 0;
-  var tahun2 = 0;
+//   var tahun1 = 0;
+//   var tahun2 = 0;
 
-  if (tahun == "1 Tahun") {
-    var tahun1 = tahunpajak - 1;
-    var tahun2 = tahunpajak - 1;
-  } else if (tahun == "3 Tahun") {
-    var tahun1 = tahunpajak - 3;
-    var tahun2 = tahunpajak - 1;
-  } else if (tahun == "5 Tahun") {
-    var tahun1 = tahunpajak - 5;
-    var tahun2 = tahunpajak - 1;
-  } else {
-    var tahun1 = tahunpajak;
-    var tahun2 = tahunpajak;
-  }
+//   if (tahun == "1 Tahun") {
+//     var tahun1 = tahunpajak - 1;
+//     var tahun2 = tahunpajak - 1;
+//   } else if (tahun == "3 Tahun") {
+//     var tahun1 = tahunpajak - 3;
+//     var tahun2 = tahunpajak - 1;
+//   } else if (tahun == "5 Tahun") {
+//     var tahun1 = tahunpajak - 5;
+//     var tahun2 = tahunpajak - 1;
+//   } else {
+//     var tahun1 = tahunpajak;
+//     var tahun2 = tahunpajak;
+//   }
 
-  var penjualan = document.getElementById("penjualan").ej2_instances[0];
-  var hargapokokpenjualan = document.getElementById("hargapokokpenjualan")
-    .ej2_instances[0];
-  var bebanoperasional =
-    document.getElementById("bebanoperasional").ej2_instances[0];
-  var namaperusahaan =
-    document.getElementById("namaperusahaan").ej2_instances[0].value;
+//   var penjualan = document.getElementById("penjualan").ej2_instances[0];
+//   var hargapokokpenjualan = document.getElementById("hargapokokpenjualan")
+//     .ej2_instances[0];
+//   var bebanoperasional =
+//     document.getElementById("bebanoperasional").ej2_instances[0];
+//   var namaperusahaan =
+//     document.getElementById("namaperusahaan").ej2_instances[0].value;
 
-  // var labakotor = document.getElementById("labakotor").ej2_instances[0];
-  // var labaoperasional = document.getElementById("labaoperasional").ej2_instances[0];
+//   // var labakotor = document.getElementById("labakotor").ej2_instances[0];
+//   // var labaoperasional = document.getElementById("labaoperasional").ej2_instances[0];
 
-  // labakotor.value = penjualan.value - hargapokokpenjualan.value;
-  // labaoperasional.value = penjualan.value - hargapokokpenjualan.value - bebanoperasional.value;
+//   // labakotor.value = penjualan.value - hargapokokpenjualan.value;
+//   // labaoperasional.value = penjualan.value - hargapokokpenjualan.value - bebanoperasional.value;
 
-  if (
-    penjualan.value == null ||
-    hargapokokpenjualan.value == null ||
-    bebanoperasional.value == null ||
-    namaperusahaan == null
-  ) {
-    toastObj.show();
-  } else if (tahun1 >= 2017) {
-    // fetch('@Url.Action("Hitung", "Service")' + '?rasio=' + rasio.value + '&jenis=' + jenis.value + '&klasifikasi=' + klasifikasi.value + '&tahun1=' + tahunslider.value[0] + '&tahun2=' + tahunslider.value[1])
-    fetch(
-      "/Service/Hitung" +
-        "?rasio=" +
-        rasio.value +
-        "&jenis=" +
-        jenis.value +
-        "&klasifikasi=" +
-        klasifikasi.value +
-        "&tahun1=" +
-        tahun1 +
-        "&tahun2=" +
-        tahun2
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.need_login != null) {
-          $("#loginModal").modal("show");
-          return;
-        }
+//   if (
+//     penjualan.value == null ||
+//     hargapokokpenjualan.value == null ||
+//     bebanoperasional.value == null ||
+//     namaperusahaan == null
+//   ) {
+//     toastObj.show();
+//   } else if (tahun1 >= 2017) {
+//     // fetch('@Url.Action("Hitung", "Service")' + '?rasio=' + rasio.value + '&jenis=' + jenis.value + '&klasifikasi=' + klasifikasi.value + '&tahun1=' + tahunslider.value[0] + '&tahun2=' + tahunslider.value[1])
+//     fetch(
+//       "/Service/Hitung" +
+//         "?rasio=" +
+//         rasio.value +
+//         "&jenis=" +
+//         jenis.value +
+//         "&klasifikasi=" +
+//         klasifikasi.value +
+//         "&tahun1=" +
+//         tahun1 +
+//         "&tahun2=" +
+//         tahun2
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         if (data.need_login != null) {
+//           $("#loginModal").modal("show");
+//           return;
+//         }
 
-        //grid.changeDataSource(data);
-        //grid.refresh
-        $("#grid-header").empty();
-        $("#grid-body").empty();
+//         //grid.changeDataSource(data);
+//         //grid.refresh
+//         $("#grid-header").empty();
+//         $("#grid-body").empty();
 
-        if (data.length != 0) {
-          let header = data[0];
-          $.each(header, function (key, value) {
-            $("#grid-header").append(
-              `<th ${
-                key == "Nama Perusahaan" || key == "Negara" ? 'width="40%"' : ""
-              }>${key}</th>`
-            );
-          });
-          data.forEach((items) => {
-            // Buat variabel untuk menyimpan baris tabel
-            let row = "<tr>";
+//         if (data.length != 0) {
+//           let header = data[0];
+//           $.each(header, function (key, value) {
+//             $("#grid-header").append(
+//               `<th ${
+//                 key == "Nama Perusahaan" || key == "Negara" ? 'width="40%"' : ""
+//               }>${key}</th>`
+//             );
+//           });
+//           data.forEach((items) => {
+//             // Buat variabel untuk menyimpan baris tabel
+//             let row = "<tr>";
 
-            // Iterasi melalui item
-            let i = 0;
-            $.each(items, function (key, value) {
-              // Tambahkan setiap nilai sebagai kolom tabel (td) ke dalam baris
-              if (i == 0 && !$("#isLogin").length) {
-                row += `<td>${maskName(value)}</td>`;
-              } else {
-                row += `<td>${value}</td>`;
-              }
-              i++;
-            });
+//             // Iterasi melalui item
+//             let i = 0;
+//             $.each(items, function (key, value) {
+//               // Tambahkan setiap nilai sebagai kolom tabel (td) ke dalam baris
+//               if (i == 0 && !$("#isLogin").length) {
+//                 row += `<td>${maskName(value)}</td>`;
+//               } else {
+//                 row += `<td>${value}</td>`;
+//               }
+//               i++;
+//             });
 
-            // Tutup tag <tr>
-            row += "</tr>";
+//             // Tutup tag <tr>
+//             row += "</tr>";
 
-            // Tambahkan seluruh baris ke dalam tbody tabel
-            $("#grid-body").append(row);
-          });
-          const elements = document.querySelectorAll(".to-hide"); // Only target elements with the 'to-hide' class
-          elements.forEach((element) => {
-            element.classList.remove("hidden");
-          });
-          const elements2 = document.querySelectorAll(".to-show"); // Only target elements with the 'to-hide' class
-          elements2.forEach((elements2) => {
-            elements2.classList.add("hidden");
-          });
-        } else {
-          toastObj.show();
-        }
-      });
+//             // Tambahkan seluruh baris ke dalam tbody tabel
+//             $("#grid-body").append(row);
+//           });
+//           const elements = document.querySelectorAll(".to-hide"); // Only target elements with the 'to-hide' class
+//           elements.forEach((element) => {
+//             element.classList.remove("hidden");
+//           });
+//           const elements2 = document.querySelectorAll(".to-show"); // Only target elements with the 'to-hide' class
+//           elements2.forEach((elements2) => {
+//             elements2.classList.add("hidden");
+//           });
+//         } else {
+//           toastObj.show();
+//         }
+//       });
 
-    //var grid2 = document.getElementById("Grid2").ej2_instances[0];
-    //fetch('@Url.Action("Hitung2", "Service")' + '?rasio=' + rasio.value + '&jenis=' + jenis.value + '&klasifikasi=' + klasifikasi.value + '&tahun1=' + tahunslider.value[0] + '&tahun2=' + tahunslider.value[1])
-    fetch(
-      "/Service/Hitung2" +
-        "?rasio=" +
-        rasio.value +
-        "&jenis=" +
-        jenis.value +
-        "&klasifikasi=" +
-        klasifikasi.value +
-        "&tahun1=" +
-        tahun1 +
-        "&tahun2=" +
-        tahun2
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        //grid2.changeDataSource(data);
-        //grid2.refresh();
+//     //var grid2 = document.getElementById("Grid2").ej2_instances[0];
+//     //fetch('@Url.Action("Hitung2", "Service")' + '?rasio=' + rasio.value + '&jenis=' + jenis.value + '&klasifikasi=' + klasifikasi.value + '&tahun1=' + tahunslider.value[0] + '&tahun2=' + tahunslider.value[1])
+//     fetch(
+//       "/Service/Hitung2" +
+//         "?rasio=" +
+//         rasio.value +
+//         "&jenis=" +
+//         jenis.value +
+//         "&klasifikasi=" +
+//         klasifikasi.value +
+//         "&tahun1=" +
+//         tahun1 +
+//         "&tahun2=" +
+//         tahun2
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         //grid2.changeDataSource(data);
+//         //grid2.refresh();
 
-        $("#grid2-header").empty();
-        $("#grid2-body").empty();
+//         $("#grid2-header").empty();
+//         $("#grid2-body").empty();
 
-        // let header = data[0];
-        let header = data && data.length > 0 ? data[0] : null;
-        if (header) {
-          $.each(header, function (key, value) {
-            $("#grid2-header").append(
-              `<th ${key == "Keterangan" ? 'width="80%"' : ""}>${key}</th>`
-            );
-          });
-        } else {
-          let modal = document.getElementById("myModal");
-          modal.style.display = "block";
-          setTimeout(function () {
-            modal.style.display = "none";
-          }, 5000);
+//         // let header = data[0];
+//         let header = data && data.length > 0 ? data[0] : null;
+//         if (header) {
+//           $.each(header, function (key, value) {
+//             $("#grid2-header").append(
+//               `<th ${key == "Keterangan" ? 'width="80%"' : ""}>${key}</th>`
+//             );
+//           });
+//         } else {
+//           let modal = document.getElementById("myModal");
+//           modal.style.display = "block";
+//           setTimeout(function () {
+//             modal.style.display = "none";
+//           }, 5000);
 
-          // Tombol close
-          let span = modal.getElementsByClassName("close")[0];
-          span.onclick = function () {
-            modal.style.display = "none";
-          };
+//           // Tombol close
+//           let span = modal.getElementsByClassName("close")[0];
+//           span.onclick = function () {
+//             modal.style.display = "none";
+//           };
 
-          // Klik di luar modal
-          window.onclick = function (event) {
-            if (event.target == modal) {
-              modal.style.display = "none";
-            }
-          };
-        }
-        data.forEach((items) => {
-          // Buat variabel untuk menyimpan baris tabel
-          let row = "<tr>";
+//           // Klik di luar modal
+//           window.onclick = function (event) {
+//             if (event.target == modal) {
+//               modal.style.display = "none";
+//             }
+//           };
+//         }
+//         data.forEach((items) => {
+//           // Buat variabel untuk menyimpan baris tabel
+//           let row = "<tr>";
 
-          // Iterasi melalui item
-          $.each(items, function (key, value) {
-            // Tambahkan setiap nilai sebagai kolom tabel (td) ke dalam baris
-            row += `<td>${value}</td>`;
-          });
+//           // Iterasi melalui item
+//           $.each(items, function (key, value) {
+//             // Tambahkan setiap nilai sebagai kolom tabel (td) ke dalam baris
+//             row += `<td>${value}</td>`;
+//           });
 
-          // Tutup tag <tr>
-          row += "</tr>";
+//           // Tutup tag <tr>
+//           row += "</tr>";
 
-          // Tambahkan seluruh baris ke dalam tbody tabel
-          $("#grid2-body").append(row);
-        });
-      });
+//           // Tambahkan seluruh baris ke dalam tbody tabel
+//           $("#grid2-body").append(row);
+//         });
+//       });
 
-    var labakotor = document.getElementById("labakotor").ej2_instances[0];
-    var labaoperasional =
-      document.getElementById("labaoperasional").ej2_instances[0];
+//     var labakotor = document.getElementById("labakotor").ej2_instances[0];
+//     var labaoperasional =
+//       document.getElementById("labaoperasional").ej2_instances[0];
 
-    labakotor.value = penjualan.value - hargapokokpenjualan.value;
-    labaoperasional.value =
-      penjualan.value - hargapokokpenjualan.value - bebanoperasional.value;
+//     labakotor.value = penjualan.value - hargapokokpenjualan.value;
+//     labaoperasional.value =
+//       penjualan.value - hargapokokpenjualan.value - bebanoperasional.value;
 
-    var testedparty = document.getElementById("testedparty").ej2_instances[0];
+//     var testedparty = document.getElementById("testedparty").ej2_instances[0];
 
-    if (
-      !penjualan.value ||
-      !hargapokokpenjualan.value ||
-      !bebanoperasional.value
-    ) {
-      testedparty.value = 0;
-    } else if (
-      rasio.value == "Gross Profit" ||
-      rasio.value == "Resale Price Method"
-    ) {
-      testedparty.value =
-        (penjualan.value - hargapokokpenjualan.value) / penjualan.value;
-      testedparty.value = Math.round(testedparty.value * 100) / 100;
-    } else if (
-      rasio.value == "Gross Profit Mark Up" ||
-      rasio.value == "Cost Plus Method"
-    ) {
-      testedparty.value =
-        (penjualan.value - hargapokokpenjualan.value) /
-        hargapokokpenjualan.value;
-      testedparty.value = Math.round(testedparty.value * 100) / 100;
-    } else if (
-      rasio.value == "Net Cost Plus Method" ||
-      rasio.value == "Net Cost Plus Method"
-    ) {
-      testedparty.value =
-        (penjualan.value - hargapokokpenjualan.value - bebanoperasional.value) /
-        (hargapokokpenjualan.value + bebanoperasional.value);
-      testedparty.value = Math.round(testedparty.value * 100) / 100;
-    } else if (rasio.value == "Return On Sales") {
-      testedparty.value =
-        (penjualan.value - hargapokokpenjualan.value - bebanoperasional.value) /
-        penjualan.value;
-      testedparty.value = testedparty.value.toFixed(2);
-    }
-    document.getElementById("jeniskegiatanusaharesult").innerHTML = jenis.value;
-    document.getElementById("klasifikasiusaharesult").innerHTML =
-      klasifikasi.value;
-    document.getElementById("subklasifikasiusaharesult").innerHTML =
-      subklasifikasi.value;
-    document.getElementById("ratioresult").innerHTML = rasio.value;
-    document.getElementById("testedpartyresult").innerHTML =
-      testedparty.value + " %";
-    document.getElementById("tahunpajakresult").innerHTML = tahunpajak;
-  } else {
-    toastObj.show();
-  }
-}
+//     if (
+//       !penjualan.value ||
+//       !hargapokokpenjualan.value ||
+//       !bebanoperasional.value
+//     ) {
+//       testedparty.value = 0;
+//     } else if (
+//       rasio.value == "Gross Profit" ||
+//       rasio.value == "Resale Price Method"
+//     ) {
+//       testedparty.value =
+//         (penjualan.value - hargapokokpenjualan.value) / penjualan.value;
+//       testedparty.value = Math.round(testedparty.value * 100) / 100;
+//     } else if (
+//       rasio.value == "Gross Profit Mark Up" ||
+//       rasio.value == "Cost Plus Method"
+//     ) {
+//       testedparty.value =
+//         (penjualan.value - hargapokokpenjualan.value) /
+//         hargapokokpenjualan.value;
+//       testedparty.value = Math.round(testedparty.value * 100) / 100;
+//     } else if (
+//       rasio.value == "Net Cost Plus Method" ||
+//       rasio.value == "Net Cost Plus Method"
+//     ) {
+//       testedparty.value =
+//         (penjualan.value - hargapokokpenjualan.value - bebanoperasional.value) /
+//         (hargapokokpenjualan.value + bebanoperasional.value);
+//       testedparty.value = Math.round(testedparty.value * 100) / 100;
+//     } else if (rasio.value == "Return On Sales") {
+//       testedparty.value =
+//         (penjualan.value - hargapokokpenjualan.value - bebanoperasional.value) /
+//         penjualan.value;
+//       testedparty.value = testedparty.value.toFixed(2);
+//     }
+//     document.getElementById("jeniskegiatanusaharesult").innerHTML = jenis.value;
+//     document.getElementById("klasifikasiusaharesult").innerHTML =
+//       klasifikasi.value;
+//     document.getElementById("subklasifikasiusaharesult").innerHTML =
+//       subklasifikasi.value;
+//     document.getElementById("ratioresult").innerHTML = rasio.value;
+//     document.getElementById("testedpartyresult").innerHTML =
+//       testedparty.value + " %";
+//     document.getElementById("tahunpajakresult").innerHTML = tahunpajak;
+//   } else {
+//     toastObj.show();
+//   }
+// }
 
 function dataBound(args) {
   this.getColumns()[0].width = 200;
